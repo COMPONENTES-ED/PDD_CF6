baca7023ccf5606c4053e8e0245f19331672ccb8
yesnapoleondynamite <yesnapoleondynamite@gmail.com>
2025-05-26 17:01:57 -0500

    material_2

--- src/pages/Actividad.vue
+++ src/pages/Actividad.vue
@@ -10,8 +10,8 @@
               img(src="@/assets/bullets/Seleccion_Multiple.svg", alt="Texto que describa la imagen")
         .col-12.col-md-8.col-lg-9
           .titulo-segundo
-            h2 Prueba de conocimientos sobre Sistematización y documentación de datos masivos mediante métodos de analítica. 
-          p.mb-4 Validar el conocimiento adquirido sobre sistematización y documentación de datos masivos mediante métodos de analítica.  Esto se trabaja a partir de un conjunto de preguntas con el propósito de buscar una dinámica de razonamiento ágil sobre opciones cerradas y reafirmar un conocimiento declarado dentro del componente.
+            h2 Prueba de conocimientos sobre Sistematización y documentación de datos masivos mediante métodos de analítica 
+          p.mb-4 Validar el conocimiento adquirido sobre Sistematización y documentación de datos masivos mediante métodos de analítica.  Esto se trabaja a partir de un conjunto de preguntas con el propósito de buscar una dinámica de razonamiento ágil sobre opciones cerradas y reafirmar un conocimiento declarado dentro del componente.
           .tarjeta.actividad.p-3
             .row.justify-content-around.align-items-center            
               .col-sm.mb-3.mb-sm-0
